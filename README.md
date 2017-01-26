16f13ddcef3ee63480a6ba3493ccf75aa56caea0
Lajos Meszaros <lajos.meszaros@performgroup.com>
2017-01-26 16:24:32 +0100

    Initial codebase from the fork of brackets-overscroll

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# brackets-scroll-past-eof
-A plugin for Brackets which allows you to scroll below the document, stopping at the last line at top.
+# Brackets Scroll past EOF
+
+A plugin for [Brackets](https://github.com/adobe/brackets/)
+which allows you to scroll below the document, stopping at the last line at top.
+
+This plugin attempts to bring the same functionality to Brackets, as the
+ScrollPastEOF plugin does for [Notepad++](https://notepad-plus-plus.org/)
+
+The plugin is far from optimal and lack support for horizontally split screens.
---
+++ main.js
@@ -0,0 +1,54 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets, window */
+
+define(function(require, exports, module){
+	'use strict';
+	
+	function getAllPanes(){
+		return Array.from(document.querySelectorAll('#editor-holder .CodeMirror-wrap .CodeMirror-sizer>div'));
+	}
+	
+	function getLineHeight(pane){
+		var measure = pane.querySelector('.CodeMirror-measure:first-child>pre>span');
+		return parseFloat(window.getComputedStyle(measure).lineHeight);
+	}
+	
+	function getPaneHeaderHeight(pane){
+		var header = pane.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.pane-header');
+		return header.offsetHeight;
+	}
+	
+	/*
+	function getScrollAmount(pane){
+		return parseFloat(pane.style.top);
+	}
+	function setScrollAmount(pane, amount){
+		pane.style.top = amount + 'px';
+	}
+	
+	function getOffset(pane){
+		return parseFloat(pane.style.paddingBottom);
+	}
+	*/
+	
+	function setOffset(pane, amount){
+		pane.style.paddingBottom = amount + 'px';
+	}
+	
+	var WorkspaceManager = brackets.getModule('view/WorkspaceManager');
+	
+	WorkspaceManager.on('workspaceUpdateLayout', function(event, newHeight){
+		getAllPanes().forEach(function(pane){
+			// var currentScroll = getScrollAmount(pane);
+			// var currentOffset = getOffset(pane);
+			var newOffset = newHeight - getLineHeight(pane) * 2 - getPaneHeaderHeight(pane);
+			
+			setOffset(pane, newOffset);
+			
+			// BUG: we have some issues with calculating top
+			// the pane isn't the only one, which has a top, plus brackets
+			// messes around with the height
+			// setScrollAmount(pane, currentScroll - currentOffset + newOffset);
+		});
+	});
+});
